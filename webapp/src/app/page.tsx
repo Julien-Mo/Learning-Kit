55cb1d6904fa6f6f323c56950f249c161575785b
Julien Mo <julienmoprofessional@gmail.com>
2025-03-01 15:12:05 -0800

    Fix merge conflict

--- webapp/src/app/page.tsx
+++ webapp/src/app/page.tsx
@@ -17,18 +17,6 @@ export default function Home() {
     return () => socket.disconnect();
   }, []);
 
-  const [message, setMessage] = useState("Waiting for data...");
-
-  useEffect(() => {
-    const socket = io("http://localhost:3001");
-
-    socket.on("serialData", (data) => {
-      setMessage(data);
-    });
-
-    return () => socket.disconnect();
-  }, []);
-
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
