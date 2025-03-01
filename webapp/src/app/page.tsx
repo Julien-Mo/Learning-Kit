f48f5bf00f703fa4ef12bc266624c453d4d22edd
Julien Mo <julienmoprofessional@gmail.com>
2025-03-01 15:36:14 -0800

    Remove home page from PR

--- webapp/src/app/page.tsx
+++ webapp/src/app/page.tsx
@@ -1,22 +1,6 @@
-"use client";
-
-import { useEffect, useState } from "react";
-import io from "socket.io-client";
 import Image from "next/image";
 
 export default function Home() {
-  const [message, setMessage] = useState("Waiting for data...");
-
-  useEffect(() => {
-    const socket = io("http://localhost:3001");
-
-    socket.on("serialData", (data) => {
-      setMessage(data);
-    });
-
-    return () => socket.disconnect();
-  }, []);
-
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
